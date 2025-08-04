288c5b8b022af4ca98778e657714f6d2f680dc79
Rachael Sikuku <rachaelsikuku033@gmail.com>
2025-08-04 14:58:14 +0300

    calculating peremeter

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ const app = express();
 const square = require("./square"); // Here we require() the name of the file without the (optional) .js file extension
 
 console.log(`The area of a square with a width of 4 is ${square.area(4)}`);
+console.log(`The perimeter of a square with a width of 5 is ${square.perimeter(5)}`);
