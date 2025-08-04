c26531e3c18502282a869bec1fd21740cfb3b11d
Rachael Sikuku <rachaelsikuku033@gmail.com>
2025-08-04 14:53:51 +0300

    using modules

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const app = express();
+const square = require("./square"); // Here we require() the name of the file without the (optional) .js file extension
+
+console.log(`The area of a square with a width of 4 is ${square.area(4)}`);
